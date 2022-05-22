0cce775e8d600461e0033c916e1faffad52fa5b1
FlorentAzd <florent4real@yahoo.com>
2022-05-22 12:20:13 +0200

    add change

--- fastapi-back/main.py
+++ fastapi-back/main.py
@@ -1,9 +1,12 @@
 # Importation des modules fastapi
 # from cmath import log
 # from operator import index
+from email import message
+from fileinput import filename
 from typing import Optional, Union
 import bcrypt
 from fastapi import FastAPI, Response, Request
+from sqlalchemy import true
 # from requests import delete, request
 # from tables import Description
 import uvicorn
@@ -172,6 +175,52 @@ def del_user(username, password):
     with open("./JSON/Users.json", "w") as file:
         json.dump(data, file, indent=3)
 
+
+@app.get("/getMessages/{id}")
+async def get_message(id):
+    last_update = []
+    ID = int(id)
+    filename = "./JSON/Messages.json"
+    # Load the json a file
+    with open(filename, "r+") as file:
+        # Extract data in json
+        data = json.load(file)
+        # Extract new ID
+        for message in data['messages']:
+            if(message["id"] > ID):
+                # print(message)
+                last_update.append(message)
+    return last_update
+
+
+@app.post("/sendMessage")
+async def post_message(request: Request):
+    dataReceive = await request.json()
+    filename = "./JSON/Messages.json"
+    # print(dataReceive)
+    message = {
+        "id": int,
+        "content": dataReceive["content"],
+        "sender": dataReceive["username"]
+    }
+
+    # # Load the json a file
+    with open(filename, "r+") as file:
+        # Extract data in json
+        data = json.load(file)
+        # Extract last ID
+        ID = 0
+        for temp in data["messages"]:
+            ID = temp["id"]
+        message["id"] = ID+1
+        # # Append the new user data
+        data["messages"].append(message)
+        file.seek(0)
+        # # Convert into JSON
+        json.dump(data, file, indent=3)
+
+    return true
+
     # Choix du port d'execution de notre API
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=2002)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
