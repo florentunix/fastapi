0e5b0bafa6353adb483416d4c0a3399483a0289d
FlorentAzd <florent4real@yahoo.com>
2022-05-24 11:13:55 +0200

    ___

--- fastapi-front/scripts/config.js
+++ fastapi-front/scripts/config.js
@@ -1,9 +1,9 @@
 // const API_HOST = "172.20.10.3"
-const API_HOST = "127.0.0.1"
+// const API_HOST = "127.0.0.1"
 
-// const API_HOST = "8753-41-82-96-84.eu.ngrok.io"
+const API_HOST = "florentazd.cf"
 const API_PORT = 8000
-const API_PROTO = "http"
+const API_PROTO = "https"
 
 module.exports = {
     API : API_PROTO+"://"+API_HOST+":"+API_PORT
